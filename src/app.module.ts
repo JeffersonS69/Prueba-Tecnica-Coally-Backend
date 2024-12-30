90f77d4744582ccd4d10ed6b7aa6397642374504
JeffersonS69 <ibarra.jefferson1@gmail.com>
2024-12-29 21:12:55 -0500

    Eliminar el módulo de configuración de entorno y simplificar la carga de variables desde .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TasksModule } from './tasks/tasks.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
-import envs from './config/envs';
-
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env',
-      load: [envs],
       isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.MONGO_URL),
